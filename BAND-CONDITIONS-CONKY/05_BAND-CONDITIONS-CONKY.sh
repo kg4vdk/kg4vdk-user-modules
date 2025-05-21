11e12722b6075be8b7522d6b04041671afa27a65
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-05-21 20:56:18 +0000

    Update 05_BAND-CONDITIONS-CONKY.sh

--- BAND-CONDITIONS-CONKY/05_BAND-CONDITIONS-CONKY.sh
+++ BAND-CONDITIONS-CONKY/05_BAND-CONDITIONS-CONKY.sh
@@ -32,7 +32,7 @@ EOF
 sudo systemctl restart cron.service
 
 # Download first image if network is reachable
-if ping -w 10 8.8.8.8; then
+if ping -c 5 -n 8.8.8.8; then
 	wget -O /tmp/conditions.gif https://www.hamqsl.com/solar100sc.php?back=transparent
 	convert /tmp/conditions.gif /tmp/conditions.png
 fi
