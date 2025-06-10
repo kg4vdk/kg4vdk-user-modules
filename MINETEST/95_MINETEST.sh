9d6901f0ef808f8f382b05e83262bcbc9938050e
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-06-10 16:10:16 +0000

    Add files via upload

---
+++ MINETEST/95_MINETEST.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#######################
+# MINETEST QRV MODULE #
+#######################
+MODULE="MINETEST"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/USER/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+killall minetest > /dev/null 2>&1
+
+if dpkg -s minetest > /dev/null 2>&1; then
+	mkdir -p ${MODULE_DIR}/minetest
+	unlink $HOME/.minetest
+	rm -rf $HOME/.minetest
+	ln -sTf ${MODULE_DIR}/minetest $HOME/.minetest
+fi
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
+
