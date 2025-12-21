316eed618b06180ca4e24b603c6988646a0ef795
Mike Fisher <mike@kg4vdk.com>
2025-12-21 00:31:43 +0000

    Trying to lessen the module "activation" workload

--- 00_HANDOVER/00_HANDOVER.sh
+++ 00_HANDOVER/00_HANDOVER.sh
@@ -20,15 +20,28 @@ USER_MODULE_DIR="${ARCHIVE}/QRV/${MYCALL}/arcos-linux-modules/USER"
 ### MODULE COMMANDS FUNCTION ###
 module_commands () {
 
-MY_MODULE_REPO="kg4vdk-user-modules"
+MYCALL_LOWER=$(echo "${MYCALL}" | tr '[:upper:]' '[:lower:]')
+MY_MODULE_REPO="${MYCALL_LOWER}-user-modules"
 
-notify-send --icon=info "${MODULE_NAME}" "${MODULE_NAME} started (POST)"
+if [ -f "${MODULE_DIR}/ENABLED_MODULES" ]; then
+    ENABLED_MODULES="$(cat ${MODULE_DIR}/ENABLED_MODULES)"
+else
+    ENABLED_MODULES=""
+fi
 
-for i in $(ls "${USER_MODULE_DIR}/${MY_MODULE_REPO}/*.sh" | grep -v "_PRE_"); do
-    MODULE_NAME=$(basename $i)
-    echo "${MODULE_NAME}"
-    bash $i
-done
+if [ -n "${ENABLED_MODULES}" ]; then
+    for i in $(echo "${ENABLED_MODULES}" | grep -v "_PRE_"); do
+        MODULE_SCRIPT="$i"
+        MODULE_SCRIPT_FULL="$(find "${USER_MODULE_DIR}/${MY_MODULE_REPO}" "$i")"
+        if [ -f "${MODULE_SCRIPT_FULL}" ]; then
+            echo -n "Running ${MODULE_SCRIPT}..."
+            bash "${MODULE_SCRIPT_FULL}"
+            echo "DONE!"
+        else
+            echo "${MODULE_SCRIPT} not found!"
+        fi
+    done
+fi
 
 } # END OF MODULE COMMANDS FUNCTION
 
