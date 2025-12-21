dc1c941e64223ca51afa45da53c8526b928ac243
KG4VDK <kg4vdk@arcOS.local>
2025-12-21 05:02:35 +0000

    Get torrent loading properly if not loaded before

--- TRANSMISSION/30_TRANSMISSION.sh
+++ TRANSMISSION/30_TRANSMISSION.sh
@@ -20,7 +20,19 @@ LOGFILE="${MODULE_DIR}/${MODULE_NAME}.log"
 module_commands () {
 
 SAVE_DIR="${ARCHIVE}/QRV/${MYCALL}/SAVED/${MODULE_NAME}"
-mkdir -p "${SAVE_DIR}/transmission"
+mkdir -p "${SAVE_DIR}/transmission"/{"resume","torrents"}
+
+if [ ! -f "${SAVE_DIR}/transmission/settings.json" ]; then
+	cp "${MODULE_DIR}/config/settings.json" "${SAVE_DIR}/transmission/settings.json"
+fi
+
+if [ ! -f "${SAVE_DIR}/transmission/torrents/8242f84b2f8c6fabddc639a485d77f66ac702d50.torrent" ]; then
+	cp -R "${MODULE_DIR}/config/torrents" "${SAVE_DIR}/transmission/torrents/"
+fi
+
+if [ ! -f "${SAVE_DIR}/transmission/resume/8242f84b2f8c6fabddc639a485d77f66ac702d50" ]; then
+	cp -R "${MODULE_DIR}/config/resume" "${SAVE_DIR}/transmission/resume"
+fi
 
 unlink "$HOME/.config/transmission"
 rm -rf "$HOME/.config/transmission"
