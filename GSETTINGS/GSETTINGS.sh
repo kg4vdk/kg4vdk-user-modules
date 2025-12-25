05348332991d1956aae568d88d492784b4aa6e8c
kg4vdk <mike@kg4vdk.com>
2025-12-25 06:16:47 +0000

    Added GSETTINGS module

---
+++ GSETTINGS/GSETTINGS.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# MODULE NAME
+MODULE_NAME="GSETTINGS"
+
+# STATION INFO
+source "$HOME/.station-info"
+MYCALL_LOWER=$(echo "${MYCALL}" | tr '[:upper:]' '[:lower:]')
+
+# PATHS
+ARCHIVE="/arcHIVE"
+USER_MODULE_DIR="${ARCHIVE}/QRV/${MYCALL}/arcos-linux-modules/USER"
+MY_MODULE_REPO="${USER_MODULE_DIR}/${MYCALL_LOWER}-user-modules"
+MODULE_DIR="${MY_MODULE_REPO}/${MODULE_NAME}"
+LOGFILE="${MODULE_DIR}/${MODULE_NAME}.log"
+
+################################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+gsettings set org.cinnamon.desktop.background.slideshow random-order true
+gsettings set org.cinnamon.desktop.background.slideshow delay 15
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > "${LOGFILE}" 2>&1 || notify-send --icon=error "${MODULE_NAME}" "${MODULE_NAME} module failed!"
