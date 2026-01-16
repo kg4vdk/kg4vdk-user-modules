d114f0d9358606e324747b15e35398bcfe9b04b0
kg4vdk <mike@kg4vdk.com>
2026-01-16 18:16:43 +0000

    update red binder module

--- RED-BINDER/RED-BINDER.sh
+++ RED-BINDER/RED-BINDER.sh
@@ -19,22 +19,45 @@ LOGFILE="${MODULE_DIR}/${MODULE_NAME}.log"
 ### MODULE COMMANDS FUNCTION ###
 module_commands () {
 
-BINDER_PATH="$HOME/Desktop/Red Binder"
+SAVE_DIR="${ARCHIVE}/QRV/${MYCALL}/SAVED/${MODULE_NAME}"
+mkdir -p "${SAVE_DIR}"
+
+BINDER_PATH="${SAVE_DIR}/red-binder"
+
+if [ ! -d "${BINDER_PATH}" ]; then
+    cp -r "${MODULE_DIR}/red-binder" "${BINDER_PATH}"
+fi
+
 ICONS_PATH="${MODULE_DIR}/icons"
 
-rm -rf "${BINDER_PATH}"
-cp -r ${MODULE_DIR}/binder "${BINDER_PATH}"
+COLOR_01="red"
+COLOR_02="pink"
+COLOR_03="orange"
+COLOR_04="yellow"
+COLOR_05="green"
+COLOR_06="blue"
+COLOR_07="indigo"
+COLOR_08="violet"
+COLOR_09="black"
+COLOR_10="grey"
 
 gio set "${BINDER_PATH}" metadata::custom-icon file://"${ICONS_PATH}"/folders/red-folder-star.png && touch "${BINDER_PATH}"
+gio set "${BINDER_PATH}"/01* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_01}"-folder.png && touch "${BINDER_PATH}"/01*
+gio set "${BINDER_PATH}"/02* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_02}"-folder.png && touch "${BINDER_PATH}"/02*
+gio set "${BINDER_PATH}"/03* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_03}"-folder.png && touch "${BINDER_PATH}"/03*
+gio set "${BINDER_PATH}"/04* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_04}"-folder.png && touch "${BINDER_PATH}"/04*
+gio set "${BINDER_PATH}"/05* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_05}"-folder.png && touch "${BINDER_PATH}"/05*
+gio set "${BINDER_PATH}"/06* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_06}"-folder.png && touch "${BINDER_PATH}"/06*
+gio set "${BINDER_PATH}"/07* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_07}"-folder.png && touch "${BINDER_PATH}"/07*
+gio set "${BINDER_PATH}"/08* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_08}"-folder.png && touch "${BINDER_PATH}"/08*
+gio set "${BINDER_PATH}"/09* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_09}"-folder.png && touch "${BINDER_PATH}"/09*
+gio set "${BINDER_PATH}"/10* metadata::custom-icon file://"${ICONS_PATH}"/folders/"${COLOR_10}"-folder.png && touch "${BINDER_PATH}"/10*
 
-gio set "${BINDER_PATH}"/01* metadata::custom-icon file://"${ICONS_PATH}"/folders/yellow-folder.png && touch "${BINDER_PATH}"/01*
-gio set "${BINDER_PATH}"/02* metadata::custom-icon file://"${ICONS_PATH}"/folders/orange-folder.png && touch "${BINDER_PATH}"/02*
-gio set "${BINDER_PATH}"/03* metadata::custom-icon file://"${ICONS_PATH}"/folders/red-folder.png && touch "${BINDER_PATH}"/03*
-gio set "${BINDER_PATH}"/04* metadata::custom-icon file://"${ICONS_PATH}"/folders/pink-folder.png && touch "${BINDER_PATH}"/04*
-gio set "${BINDER_PATH}"/05* metadata::custom-icon file://"${ICONS_PATH}"/folders/violet-folder.png && touch "${BINDER_PATH}"/05*
-gio set "${BINDER_PATH}"/06* metadata::custom-icon file://"${ICONS_PATH}"/folders/indigo-folder.png && touch "${BINDER_PATH}"/06*
-gio set "${BINDER_PATH}"/07* metadata::custom-icon file://"${ICONS_PATH}"/folders/blue-folder.png && touch "${BINDER_PATH}"/07*
-gio set "${BINDER_PATH}"/08* metadata::custom-icon file://"${ICONS_PATH}"/folders/green-folder.png && touch "${BINDER_PATH}"/08*
+LINK_PATH="$HOME/Desktop/Red Binder"
+
+unlink "${LINK_PATH}"
+rm -rf "${LINK_PATH}"
+ln -sTf "${BINDER_PATH}" "${LINK_PATH}"
 
 } # END OF MODULE COMMANDS FUNCTION
 
