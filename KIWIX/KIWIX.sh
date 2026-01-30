ab74192be58f2832393689875b29173cb0899c2b
kg4vdk <mike@kg4vdk.com>
2026-01-30 06:09:39 +0000

    Add KIWIX module

---
+++ KIWIX/KIWIX.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# MODULE NAME
+MODULE_NAME="KIWIX"
+
+# STATION INFO
+source "$HOME/.station-info"
+MYCALL_LOWER=$(echo "${MYCALL}" | tr '[:upper:]' '[:lower:]')
+
+# PATHS
+ARCHIVE="/arcHIVE"
+USER_MODULE_DIR="${ARCHIVE}/QRV/${MYCALL}/arcos-linux-modules/USER"
+MY_MODULE_REPO="${USER_MODULE_DIR}/${MYCALL_LOWER}-user-modules"
+MODULE_DIR="${MY_MODULE_REPO}/${MODULE_NAME}"
+LOGFILE="${MODULE_DIR}/${MODULE_NAME}.log"
+
+################################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+$MODULE_DIR/kiwix-tools/kiwix-serve -d -p 1234 -M $ARCHIVE/QRV/.zim/*.zim
+
+cp $MODULE_DIR/applications/kiwix.desktop $HOME/.local/share/applications/
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || echo "$MODULE_NAME" >> /tmp/.failed-modules.log
