64f0547d1a41857aca97c61505e8d2a49a742660
kg4vdk <mike@kg4vdk.com>
2026-04-09 03:10:01 +0000

    add monitor handling

---
+++ MONITORS/MONITORS.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# MODULE NAME
+MODULE_NAME="MONITORS"
+
+# STATION INFO
+source "$HOME/.station-info"
+MYCALL_LOWER=$(echo "${MYCALL}" | tr '[:upper:]' '[:lower:]')
+
+# PATHS
+ARCHIVE="/arcHIVE"
+USER_MODULE_DIR="${ARCHIVE}/QRV/${MYCALL}/arcos-linux-modules/USER"
+MY_MODULE_REPO="${USER_MODULE_DIR}/${MYCALL_LOWER}-user-modules"
+MODULE_DIR="${MY_MODULE_REPO}/${MODULE_NAME}"
+LOGFILE="${MODULE_DIR}/${MODULE_NAME}.log"
+
+################################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+SAVE_DIR="${ARCHIVE}/QRV/${MYCALL}/SAVED/${MODULE_NAME}"
+mkdir -p "${SAVE_DIR}"
+
+if [ -f $SAVE_DIR/cinnamon-monitors.xml ]; then
+	rm $HOME/.config/cinnamon-monitors.xml
+	ln -sf $SAVE_DIR/cinnamon-monitors.xml $HOME/.config/cinnamon-monitors.xml
+else
+	touch $SAVE_DIR/cinnamon-monitors.xml
+	rm $HOME/.config/cinnamon-monitors.xml
+	ln -sf $SAVE_DIR/cinnamon-monitors.xml $HOME/.config/cinnamon-monitors.xml
+fi
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || echo "$MODULE_NAME" >> /tmp/.failed-modules.log
